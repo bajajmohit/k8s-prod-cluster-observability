422894809c1bc195413878f907df311f112d5ffe
bajajmohit <bajajmohit08@gmail.com>
2025-07-05 12:40:42 +0400

    Add EKS cluster configuration in terraform/eks.tf
    - Defined EKS module with cluster settings, including version, subnet IDs, and node group configurations.
    - Added outputs for cluster endpoint, security group ID, IAM role name, certificate authority data, cluster ID, OIDC issuer URL, and primary security group ID.

---
+++ terraform/eks.tf
@@ -0,0 +1,62 @@
+# EKS Cluster Configuration
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.8.4"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = var.desired_capacity
+      max_size     = var.max_size
+      min_size     = var.min_size
+
+      instance_types = [var.instance_type]
+      capacity_type  = "ON_DEMAND"
+    }
+  }
+
+  tags = {
+    Environment = "prod"
+    Terraform   = "true"
+  }
+}
+
+# EKS Cluster Outputs
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "cluster_iam_role_name" {
+  description = "IAM role name associated with EKS cluster"
+  value       = module.eks.cluster_iam_role_name
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = module.eks.cluster_certificate_authority_data
+}
+
+output "cluster_id" {
+  description = "EKS cluster ID"
+  value       = module.eks.cluster_id
+}
+
+output "cluster_oidc_issuer_url" {
+  description = "The URL on the EKS cluster for the OpenID Connect identity provider"
+  value       = module.eks.cluster_oidc_issuer_url
+}
+
+output "cluster_primary_security_group_id" {
+  description = "Cluster security group that was created by Amazon EKS for the cluster"
+  value       = module.eks.cluster_primary_security_group_id
+}
